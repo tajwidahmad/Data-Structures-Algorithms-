64a7cc2d931600f9efe4a46bb1debbdf0321f89c
TAJWID AHMAD <tajwidahmad5@gmail.com>
2025-12-11 14:54:35 +0000

    this is an introductory push

---
+++ intro_to_.java
@@ -0,0 +1,6 @@
+public class intro_to_arrays
+{
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
---
+++ intro_to_arrays.java
@@ -0,0 +1,5 @@
+public class intro_to_arrays{
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
