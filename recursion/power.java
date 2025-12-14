777f2b449e18f24627c932b00b81789a20d56cd2
TAJWID AHMAD <tajwidahmad5@gmail.com>
2025-12-14 14:51:02 +0000

    optimised power

--- recursion/power.java
+++ recursion/power.java
@@ -2,15 +2,36 @@ package recursion;
 
 public class power {
     public static void main(String[] args) {
-        System.out.println(x_t_p(2, 10));
+        // System.out.println(x_t_p(2, 10));
+        System.out.println(optimzed_power(2, 5));
         
     }
-    public static int x_t_p(int x, int n) {
-        if(n==1){
-            return x;
+    // public static int x_t_p(int x, int n) {
+    //     if(n==1){
+    //         return x;
+    //     }
+    //     int power_of_x=x*x_t_p(x, n-1);
+    //     return power_of_x;
+        
+    // }
+
+    //################### optimized ######################
+    public static int optimzed_power(int x,int n) {
+        // base case
+        if (n==0) {
+            return 1;
         }
-        int power_of_x=x*x_t_p(x, n-1);
-        return power_of_x;
+        // int final_power=optimzed_power(x, n/2)*optimzed_power(x, n/2);
+        // ######## more optimixe it
+        int half_power=optimzed_power(x, n/2);
+        int half_power_square=half_power*half_power;
+
+        // odd hoga agar hamara n ->
+        if (n%2!=0) {
+            return x*half_power_square;
+            
+        }
+        return half_power_square;
         
     }
 }
