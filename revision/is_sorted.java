f94aede90e54fc6d028f4932bb6ec6c219437b1d
TAJWID AHMAD <tajwidahmad5@gmail.com>
2026-01-08 14:28:23 +0000

    this is another recursive push

--- revision/is_sorted.java
+++ revision/is_sorted.java
@@ -1,19 +1,34 @@
 package revision;
 
 public class is_sorted {
+    // public static void main(String[] args) {
+    //     int arr[]={1,2,3,4,5};
+    //     System.out.println(checking_sorted(arr, 0));
+    // }
+    // public static boolean checking_sorted(int arr[],int idx) {
+    //     // base case
+    //     if (idx==arr.length-1) {
+    //         return true;
+    //     }
+    //     if (arr[idx]>arr[idx+1]) {
+    //         return false;
+    //     }
+    //     return checking_sorted(arr, idx+1);
+        
+    // }
+    // first occurence...
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5};
-        System.out.println(checking_sorted(arr, 0));
+        int arr[]={3,4,6,8,2,6,1,5};
+        System.out.println(first_time(arr, 0, 6));
     }
-    public static boolean checking_sorted(int arr[],int idx) {
+    public static int first_time(int arr[],int idx,int target) {
         // base case
         if (idx==arr.length-1) {
-            return true;
+            return -1;
         }
-        if (arr[idx]>arr[idx+1]) {
-            return false;
+        if (arr[idx]==target) {
+            return idx;
         }
-        return checking_sorted(arr, idx+1);
-        
+        return first_time(arr, idx+1, target);
     }
 }
